9db1113be42c67076844dea23fc661a477ccb48b
sorenjo <serenjohanse@yahoo.com>
2021-12-09 15:19:01 +0100

    All move tests

--- del2/Test.java
+++ del2/Test.java
@@ -1,9 +1,69 @@
 public class Test {
   public static void main(String[] args) {
-    for (int i = 1; i <= 25; i++)
-        for (int j = 1; j <= 25; j++)
-            if (i != j)
-                TestBoardMoves(new int[]{j}, new int[]{i}, Board.piece.BLACK);
+
+    // test of moves in the first and fifth columns
+    /*
+    for (int i = 1; i <= 21; i = i + 5)
+      TestBoardMoves(new int[]{}, new int[]{i}, Board.piece.BLACK);
+
+    for (int i = 25; i >= 5; i = i - 5)
+      TestBoardMoves(new int[]{i}, new int[]{}, Board.piece.WHITE);
+
+    for (int i = 5; i <= 25; i = i + 5)
+      TestBoardMoves(new int[]{}, new int[]{i}, Board.piece.BLACK);
+    */
+
+    // test of moves in the second, third or fourth columns
+    /*
+    for (int i = 2; i <= 24; i = i + 5)
+      TestBoardMoves(new int[]{}, new int[]{i}, Board.piece.BLACK);
+    */
+
+    // test of axial attack moves in
+    // first column
+    /*
+    for (int i = 1; i <= 21; i = i + 5)
+      TestBoardMoves(new int[]{i+1}, new int[]{i}, Board.piece.BLACK);
+
+    for (int i = 1; i <= 16; i = i + 5)
+      TestBoardMoves(new int[]{i+5}, new int[]{i}, Board.piece.BLACK);
+
+    // second column
+    for (int i = 2; i <= 22; i = i + 5)
+      TestBoardMoves(new int[]{i+1, i-1}, new int[]{i}, Board.piece.BLACK);
+
+    for (int i = 2; i <= 17; i = i + 5)
+      TestBoardMoves(new int[]{i+5}, new int[]{i}, Board.piece.BLACK);
+
+    //third column
+    for (int i = 3; i <= 23; i = i + 5)
+      TestBoardMoves(new int[]{i+1, i-1}, new int[]{i}, Board.piece.BLACK);
+
+    for (int i = 3; i <= 18; i = i + 5)
+      TestBoardMoves(new int[]{i+5}, new int[]{i}, Board.piece.BLACK);
+    */
+
+    // test of diagonal attack moves in
+    // first column
+    for (int i = 1; i <= 16; i = i + 5)
+      TestBoardMoves(new int[]{i+6}, new int[]{i}, Board.piece.BLACK);
+
+    TestBoardMoves(new int[]{7,17}, new int[]{11}, Board.piece.BLACK);
+
+    // second column
+    for (int i = 2; i <= 17; i = i + 5)
+      TestBoardMoves(new int[]{i+6}, new int[]{i}, Board.piece.BLACK);
+
+    //third column
+    for (int i = 3; i <= 18; i = i + 5)
+      TestBoardMoves(new int[]{i+6, i+4}, new int[]{i}, Board.piece.BLACK);
+    TestBoardMoves(new int[]{7,9,17,19}, new int[]{13}, Board.piece.BLACK);
+
+
+    /*
+    for (int i = 5; i <= 25; i = i + 5)
+      TestBoardMoves(new int[]{}, new int[]{i}, Board.piece.BLACK);
+    */
   }
 
 
