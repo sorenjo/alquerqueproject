1863bf2369a501240abd8088ad08802cf0c227de
sej <sej@sejdt.localhost>
2021-12-31 11:47:39 +0100

    Improved doc

--- del3/Minimax.java
+++ del3/Minimax.java
@@ -1,4 +1,8 @@
 public class Minimax{
+
+    /*
+     * Creates a minimax tree for the given board with the given depth and returns the best move for that board
+     */
     public static Move nextMove(Board board, int depth, boolean isWhite){
         MinimaxTree boardTree = new MinimaxTree(board, depth, isWhite);
         return boardTree.nextMove();
